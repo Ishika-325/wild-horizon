03e5ee8aab96e0f3565c689823068361031737fb
Ishika-325 <gishika325@gmail.com>
2025-08-01 17:49:04 +0530

    created server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("hii! everyone")
+import http from 'node:http' 
+const PORT = 8000;
+
+
+const server = http.createServer((req, res) => {
+    res.end("hello from server side");
+})
+
+server.listen(PORT, () => {
+    console.log(`server is running on the port: ${PORT}`)
+})
