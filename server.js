9f653404865f0ebd14ec76202628db05d2d2b4ce
Ishika-325 <gishika325@gmail.com>
2025-08-01 18:19:42 +0530

    day 1 of learning node js end

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+import { readlinkSync } from 'node:fs';
 import http from 'node:http' 
 const PORT = 8000;
 
 
 const server = http.createServer((req, res) => {
-    res.end("hello from server side");
-})
+    res.write("my data starts here \n")
+    res.write("this is second line \n")
+    res.end()
+});
+
 
 server.listen(PORT, () => {
     console.log(`server is running on the port: ${PORT}`)
